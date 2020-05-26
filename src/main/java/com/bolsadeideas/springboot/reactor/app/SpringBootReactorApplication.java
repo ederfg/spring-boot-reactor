d20473d41a1d6caee8ab1260e9f3667c83a3efbc
Eder Flores <eder2006.10@gmail.com>
2020-05-26 15:16:09 -0500

    Pasando a la clase 15

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -9,15 +9,16 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bolsadeideas.springboot.reactor.app.models.Comentarios;
 import com.bolsadeideas.springboot.reactor.app.models.Usuario;
+import com.bolsadeideas.springboot.reactor.app.models.UsuarioComentarios;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-public class SpringBootReactorApplication implements CommandLineRunner{
-	
-	
+public class SpringBootReactorApplication implements CommandLineRunner {
+
 	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 
 	public static void main(String[] args) {
@@ -27,68 +28,102 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		//System.out.println("Haola mundo");
-		
-		//ejemploIterable(args);
-		//ejemploFlatMap();
-		//ejemploToString();
-		ejemploToCollectList();
+		// System.out.println("Haola mundo");
+
+		// ejemploIterable(args);
+		// ejemploFlatMap();
+		// ejemploToString();
+		//ejemploToCollectList();
+		//ejemploUsuarioComentariosFlatMap();
+		ejemploUsuarioComentariosZipWith();
 	}
 	
+	public void ejemploUsuarioComentariosZipWithForma2() {
+		Mono<Usuario> usuarioMono = Mono.fromCallable(()-> new Usuario("John","Doe"));
+		
+		Mono<Comentarios> comentariosUsuarioMono = Mono.fromCallable(()->{
+			Comentarios comentarios = new Comentarios();
+			comentarios.addComentario("Hola como estás");
+			comentarios.addComentario("Mañana vdoy a la playa!");
+			comentarios.addComentario("Estoy tomando el curso de spring reactivo");
+			return comentarios;
+		});
+		Mono<UsuarioComentarios> usuarioConComentarios = usuarioMono.zipWith(comentariosUsuarioMono).map(tuple->{
+			Usuario u = tuple.getT1();
+			Comentarios c = tuple.getT2();
+			return new UsuarioComentarios(u, c);
+		});
+		usuarioConComentarios.subscribe(uc-> log.info(uc.toString()));
+	}
+	
+
+	public void ejemploUsuarioComentariosZipWith() {
+		Mono<Usuario> usuarioMono = Mono.fromCallable(()-> new Usuario("John","Doe"));
+		
+		Mono<Comentarios> comentariosUsuarioMono = Mono.fromCallable(()->{
+			Comentarios comentarios = new Comentarios();
+			comentarios.addComentario("Hola como estás");
+			comentarios.addComentario("Mañana vdoy a la playa!");
+			comentarios.addComentario("Estoy tomando el curso de spring reactivo");
+			return comentarios;
+		});
+		Mono<UsuarioComentarios> usuarioConComentarios = usuarioMono.zipWith(comentariosUsuarioMono,(usuario,comentariosUsuarios)->new UsuarioComentarios(usuario,comentariosUsuarios));
+		usuarioConComentarios.subscribe(uc-> log.info(uc.toString()));
+	}
+	
+	
+	public void ejemploUsuarioComentariosFlatMap() {
+		Mono<Usuario> usuarioMono = Mono.fromCallable(()-> new Usuario("John","Doe"));
+		
+		Mono<Comentarios> comentariosUsuarioMono = Mono.fromCallable(()->{
+			Comentarios comentarios = new Comentarios();
+			comentarios.addComentario("Hola como estás");
+			comentarios.addComentario("Mañana vdoy a la playa!");
+			comentarios.addComentario("Estoy tomando el curso de spring reactivo");
+			return comentarios;
+		});
+		
+		
+		usuarioMono.flatMap( u-> comentariosUsuarioMono.map(c-> new UsuarioComentarios(u, c)))
+		.subscribe(uc-> log.info(uc.toString()));
+	}
+
 	public void ejemploToCollectList(String... args) throws Exception {
 		List<Usuario> usuariosList = new ArrayList<>();
-		usuariosList.add(new Usuario("Bernardo"," Guzman"));
-		usuariosList.add(new Usuario("Pepe" ,"Flores"));
+		usuariosList.add(new Usuario("Bernardo", " Guzman"));
+		usuariosList.add(new Usuario("Pepe", "Flores"));
 		usuariosList.add(new Usuario("Eder", "Flores"));
 		usuariosList.add(new Usuario("Julia", "Guzman"));
-		usuariosList.add(new Usuario("Maria" ,"Cabrera"));
-		usuariosList.add(new Usuario("Bruce" ,"Lee"));
-		usuariosList.add(new Usuario("Bruce" ,"Williams"));
-		
-		
-		Flux.fromIterable(usuariosList)
-				.map(elemento-> elemento.getNombre().toUpperCase()+" "+elemento.getApellido().toUpperCase())
-				.flatMap(usuario -> {
-					if(usuario.contains("bruce".toUpperCase())) {
-						return Mono.just(usuario);
-					}else {
-						return Mono.empty();
-					}
-				}  )
-				.map(usuario-> 
-					 usuario.toLowerCase()
-				).subscribe(e->log.info(e.toString())
-			);
+		usuariosList.add(new Usuario("Maria", "Cabrera"));
+		usuariosList.add(new Usuario("Bruce", "Lee"));
+		usuariosList.add(new Usuario("Bruce", "Williams"));
+
+		Flux.fromIterable(usuariosList).collectList().subscribe(lista -> {
+			lista.forEach(e -> log.info(e.toString()));
+		});
 	}
-	
-	
+
 	public void ejemploToString(String... args) throws Exception {
 		List<Usuario> usuariosList = new ArrayList<>();
-		usuariosList.add(new Usuario("Bernardo"," Guzman"));
-		usuariosList.add(new Usuario("Pepe" ,"Flores"));
+		usuariosList.add(new Usuario("Bernardo", " Guzman"));
+		usuariosList.add(new Usuario("Pepe", "Flores"));
 		usuariosList.add(new Usuario("Eder", "Flores"));
 		usuariosList.add(new Usuario("Julia", "Guzman"));
-		usuariosList.add(new Usuario("Maria" ,"Cabrera"));
-		usuariosList.add(new Usuario("Bruce" ,"Lee"));
-		usuariosList.add(new Usuario("Bruce" ,"Williams"));
-		
-		
+		usuariosList.add(new Usuario("Maria", "Cabrera"));
+		usuariosList.add(new Usuario("Bruce", "Lee"));
+		usuariosList.add(new Usuario("Bruce", "Williams"));
+
 		Flux.fromIterable(usuariosList)
-				.map(elemento-> elemento.getNombre().toUpperCase()+" "+elemento.getApellido().toUpperCase())
+				.map(elemento -> elemento.getNombre().toUpperCase() + " " + elemento.getApellido().toUpperCase())
 				.flatMap(usuario -> {
-					if(usuario.contains("bruce".toUpperCase())) {
+					if (usuario.contains("bruce".toUpperCase())) {
 						return Mono.just(usuario);
-					}else {
+					} else {
 						return Mono.empty();
 					}
-				}  )
-				.map(usuario-> 
-					 usuario.toLowerCase()
-				).subscribe(e->log.info(e.toString())
-			);
+				}).map(usuario -> usuario.toLowerCase()).subscribe(e -> log.info(e.toString()));
 	}
-	
-	
+
 	public void ejemploFlatMap(String... args) throws Exception {
 		List<String> usuariosList = new ArrayList<>();
 		usuariosList.add("Bernardo Guzman");
@@ -98,29 +133,26 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 		usuariosList.add("Maria Cabrera");
 		usuariosList.add("Bruce Lee");
 		usuariosList.add("Bruce Williams");
-		
-		
-		Flux.fromIterable(usuariosList)
-				.map(elemento-> new Usuario(elemento.split(" ")[0].toUpperCase(), elemento.split(" ")[1].toUpperCase()))
+
+		Flux.fromIterable(usuariosList).map(
+				elemento -> new Usuario(elemento.split(" ")[0].toUpperCase(), elemento.split(" ")[1].toUpperCase()))
 				.flatMap(usuario -> {
-					if(usuario.getNombre().equalsIgnoreCase("bruce")) {
+					if (usuario.getNombre().equalsIgnoreCase("bruce")) {
 						return Mono.just(usuario);
-					}else {
+					} else {
 						return Mono.empty();
 					}
-				}  )
-				.map(usuario-> { 
+				}).map(usuario -> {
 					String nombre = usuario.getNombre().toLowerCase();
 					usuario.setNombre(nombre);
 					return usuario;
-				}).subscribe(e->log.info(e.toString())
-			);
+				}).subscribe(e -> log.info(e.toString()));
 	}
-	
+
 	public void ejemploIterable(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		//System.out.println("Haola mundo");
-		
+		// System.out.println("Haola mundo");
+
 		List<String> usuariosList = new ArrayList<>();
 		usuariosList.add("Bernardo Guzman");
 		usuariosList.add("Pepe Flores");
@@ -129,30 +161,29 @@ public class SpringBootReactorApplication implements CommandLineRunner{
 		usuariosList.add("Maria Cabrera");
 		usuariosList.add("Bruce Lee");
 		usuariosList.add("Bruce Williams");
-		
+
 		Flux<String> nombres = Flux.fromIterable(usuariosList);
-		
-		Flux<Usuario> usuarios  = nombres.map(elemento-> new Usuario(elemento.split(" ")[0].toUpperCase(), elemento.split(" ")[1].toUpperCase()))
-				.filter(f -> f.getNombre().contains("B"))
-				.doOnNext(usuario->{
-					if(usuario == null) {
+
+		Flux<Usuario> usuarios = nombres
+				.map(elemento -> new Usuario(elemento.split(" ")[0].toUpperCase(),
+						elemento.split(" ")[1].toUpperCase()))
+				.filter(f -> f.getNombre().contains("B")).doOnNext(usuario -> {
+					if (usuario == null) {
 						throw new RuntimeException("El nombre no puede estár vacio");
 					}
 					System.out.println(usuario.toString());
-				})
-				.map(usuario-> { 
+				}).map(usuario -> {
 					String nombre = usuario.getNombre().toLowerCase();
 					usuario.setNombre(nombre);
 					return usuario;
 				})
-				
-				;
-		
-		usuarios.subscribe(e->log.info(e.toString()),
-				err->log.error(err.getMessage()),
-				
+
+		;
+
+		usuarios.subscribe(e -> log.info(e.toString()), err -> log.error(err.getMessage()),
+
 				new Runnable() {
-					
+
 					@Override
 					public void run() {
 						// TODO Auto-generated method stub
